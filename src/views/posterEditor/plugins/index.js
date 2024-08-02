a6d0e8e78d505686a16c310736dd6d0c192d7226
malaxiaoyuzai <1017969390@qq.com>
2024-08-02 17:29:45 +0800

    新增buttonWidget

--- src/views/posterEditor/plugins/index.js
+++ src/views/posterEditor/plugins/index.js
@@ -2,6 +2,8 @@ import { usePlugin } from './helpers'
 export { pluginMap, pluginWrap, pluginConstructorMap } from './helpers'
 
 import pluginA from './pluginA'
+import buttonWidget from './buttonWidget'
 
 usePlugin('pluginA', pluginA)
+usePlugin('buttonWidget', buttonWidget)
 
